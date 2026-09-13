7acd73ac43b09882e549e93de738851f199373e7
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-13 20:29:17 +0200

    Permettre les images locales et les cases personnalisées

--- app/src/main/java/com/chk/bluelocktier/CharacterItem.java
+++ app/src/main/java/com/chk/bluelocktier/CharacterItem.java
@@ -4,12 +4,20 @@ public final class CharacterItem {
     public final String id;
     public final String name;
     public final String rarity;
+    public final boolean custom;
     public boolean checked;
+    public String imageUri;
 
     public CharacterItem(String id, String name, String rarity) {
+        this(id, name, rarity, false);
+    }
+
+    public CharacterItem(String id, String name, String rarity, boolean custom) {
         this.id = id;
         this.name = name;
         this.rarity = rarity;
+        this.custom = custom;
         this.checked = false;
+        this.imageUri = null;
     }
 }
